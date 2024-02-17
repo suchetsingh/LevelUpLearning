0214d0b6a7d96059c5442944f84d43907a489847
suchetsingh <ssingh21be20@gmail.com>
2024-02-17 14:49:36 +0530

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechStar</h1>
+        <p>@Suchet Singh all right reserved</p>
+      </div>
+      <div>
+        <h5>Follow us</h5>
+        <div>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/suchet-singh-b9823023a/"
+          >
+            Linkdein
+          </a>
+          <a target="_blank" href="https://github.com/suchetsingh">
+            Github
+          </a>
+          <a
+            target="_blank"
+            href="https://my-portfolio-git-main-suchetsinghs-projects.vercel.app/"
+          >
+            Portfolio
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
